cefed48bfa397845e5596898500ff3e28222af5e
Célian HAMON <hamoncelian@gmail.com>
2026-02-26 10:03:33 +0100

    w

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, categories, chat, fraud, items, users
+from app.api.v1.endpoints import auth, categories, fraud, items, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
@@ -8,4 +8,3 @@ api_router.include_router(items.router, prefix="/articles", tags=["articles"])
 api_router.include_router(categories.router, prefix="/categories", tags=["categories"])
 api_router.include_router(fraud.router, prefix="/fraud-logs", tags=["fraud"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
